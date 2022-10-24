d70611f941c87309f89745585aeb2bbf5b0ba500
Jack Bui <jackbui05@hotmail.com>
2022-10-23 21:32:56 -0700

    basic input to api

---
+++ pages/api/forms.js
@@ -0,0 +1,13 @@
+
+export default function handler(req, res) {
+    const body = req.body
+    
+
+
+
+    console.log('body: ', body)
+    
+
+    res.status(200).json({ data: `${body.City} ${body.Date} ${body.Humidity}` })
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,19 @@ const Home: NextPage = () => {
         }}
       >
         <TitleBar />
-        <Box sx={{ flex: '1 1' }}>input</Box>
+        <Box sx={{ flex: '1 1' }}>input
+        
+        <form action="/api/forms" method="post">
+            <label htmlFor="City">City:</label>
+            <input type="text" id="City" name="City" /><br></br>
+            <label htmlFor="Date">Date:</label>
+            <input type="text" id="Date" name="Date" /><br></br>
+            <label htmlFor= "Humidity">Humidity</label>
+            <input type = "text" id="Humidity" name = "Humidity" /><br></br>
+            <button type="submit">Submit</button>
+          </form>
+          </Box>
+        
         <Box
           sx={{
             flex: '4 4',
@@ -28,8 +40,12 @@ const Home: NextPage = () => {
             width: '100%',
             display: 'flex',
             flexDirection: 'row',
+            
           }}
+          
+          
         >
+          
           <Box
             sx={{
               flex: '4 4',
@@ -40,7 +56,9 @@ const Home: NextPage = () => {
           >
             Map
           </Box>
-          <Box sx={{ flex: '1 1' }}>Data</Box>
+          <Box sx={{ flex: '1 1' }}>Data</Box><br></br>
+          <Box>
+        </Box>
         </Box>
       </Box>
     </div>

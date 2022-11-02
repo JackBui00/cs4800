8d4d6e624e32787db09b5e8ef89cc0e4b3fc20c4
Jack Bui <jackbui05@hotmail.com>
2022-11-01 17:58:01 -0700

    user input

--- pages/api/forms.js
+++ pages/api/forms.js
@@ -1,13 +1,48 @@
+import Papa from 'papaparse';
+import "csv-parser";
+
+
+
+
+
+const csv = 'public/fireTestData.csv'
+
+function find(date){
+    //return csvData.filter(data=> data.Date === date)[0].City
+} 
 
 export default function handler(req, res) {
     const body = req.body
+    console.log(typeof body)
+    
+    
+    const csv = require('csv-parser');
+    const fs = require('fs');
+    
+    fs.createReadStream('public/fireTestData.csv')
+        .pipe(csv())
+        .on('data', (row) => {
+        
+        //console.log(row);
+        //console.log(row)
+        
+        //console.log(row)
+        //if(row.Date === body.Date & row.City === body.City){
+        //    console.log(row)
+        //}
+        
+    })
+    .on('end', () => {
+    
+  });
     
 
 
-
-    console.log('body: ', body)
     
-
+    console.log(req.body); 
+    
+    
+    //res.status(200).json({csvData})
     res.status(200).json({ data: `${body.City} ${body.Date} ${body.Humidity}` })
 
 }

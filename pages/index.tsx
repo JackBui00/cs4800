8d77c6f504946a392cedba22942cd797f7c33e30
Jack Bui <jackbui05@hotmail.com>
2022-11-08 21:42:40 -0800

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,19 +20,22 @@ const Home: NextPage = () => {
         }}
       >
         <TitleBar />
-        <Box sx={{ flex: '1 1' }}>input
-        
-        <form action="/api/forms" method="post">
+        <Box sx={{ flex: '1 1' }}>
+          input
+          <form action="/api/forms" method="post">
             <label htmlFor="City">City:</label>
-            <input type="text" id="City" name="City" /><br></br>
+            <input type="text" id="City" name="City" />
+            <br></br>
             <label htmlFor="Date">Date:</label>
-            <input type="text" id="Date" name="Date" /><br></br>
-            <label htmlFor= "Humidity">Humidity</label>
-            <input type = "text" id="Humidity" name = "Humidity" /><br></br>
+            <input type="text" id="Date" name="Date" />
+            <br></br>
+            <label htmlFor="Humidity">Humidity</label>
+            <input type="text" id="Humidity" name="Humidity" />
+            <br></br>
             <button type="submit">Submit</button>
           </form>
-          </Box>
-        
+        </Box>
+
         <Box
           sx={{
             flex: '4 4',
@@ -40,12 +43,8 @@ const Home: NextPage = () => {
             width: '100%',
             display: 'flex',
             flexDirection: 'row',
-            
           }}
-          
-          
         >
-          
           <Box
             sx={{
               flex: '4 4',
@@ -56,9 +55,9 @@ const Home: NextPage = () => {
           >
             Map
           </Box>
-          <Box sx={{ flex: '1 1' }}>Data</Box><br></br>
-          <Box>
-        </Box>
+          <Box sx={{ flex: '1 1' }}>Data</Box>
+          <br></br>
+          <Box></Box>
         </Box>
       </Box>
     </div>

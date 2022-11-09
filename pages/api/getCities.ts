ec72d699c42bdde025aa8983be5f47d59703001c
ZombiMigz <chunyumikey@gmail.com>
2022-11-08 20:55:55 -0800

    fixes typescript bug

--- pages/api/getCities.ts
+++ pages/api/getCities.ts
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Cities, getCities } from '../../data/cityCache';
+import { City, getCities } from '../../data/cityCache';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Cities>
+  res: NextApiResponse<City[]>
 ) {
   res.status(200).json(await getCities());
 }

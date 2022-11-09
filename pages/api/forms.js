6e6b6e68ebe219939e460ab1f241054873d10181
Jack Bui <jackbui05@hotmail.com>
2022-11-08 21:42:15 -0800

    format remove spacing

--- pages/api/forms.js
+++ pages/api/forms.js
@@ -1,38 +1,27 @@
-import "csv-parser";
-
+import 'csv-parser';
 
 export default function handler(req, res) {
-    const body = req.body
-    console.log(typeof body)
-    
-    
-    const csv = require('csv-parser');
-    const fs = require('fs');
-    
-    fs.createReadStream('public/fireTestData.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-        
-        console.log(row);
-        //console.log(row)
-        
-        //console.log(row)
-        //if(row.Date === body.Date & row.City === body.City){
-        //    console.log(row)
-        //}
-        
+  const body = req.body;
+  console.log(typeof body);
+
+  const csv = require('csv-parser');
+  const fs = require('fs');
+
+  fs.createReadStream('public/fireTestData.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      console.log(row);
+      //console.log(row)
+
+      //console.log(row)
+      //if(row.Date === body.Date & row.City === body.City){
+      //    console.log(row)
+      //}
     })
-    .on('end', () => {
-    
-  });
-    
+    .on('end', () => {});
 
+  console.log(req.body);
 
-    
-    console.log(req.body); 
-    
-    
-    //res.status(200).json({csvData})
-    res.status(200).json({ data: `${body.City} ${body.Date} ${body.Humidity}` })
-
+  //res.status(200).json({csvData})
+  res.status(200).json({ data: `${body.City} ${body.Date} ${body.Humidity}` });
 }

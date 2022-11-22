637ddaf8e314f9ceb7d377562cedc4a97cde8829
JackBui00 <jackbui05@hotmail.com>
2022-11-22 12:13:54 -0800

    fix json report

--- pages/api/forms.js
+++ pages/api/forms.js
@@ -22,5 +22,5 @@ export default function handler(req, res) {
   console.log(req.body);
 
   //res.status(200).json({csvData})
-  res.status(200).json({ data: `${body.City} ${body.Date} ${body.Rainfall_30_Days} ${body.Rainfall_60_Days} ${body.Rainfall_60_Days}` });
+  res.status(200).json({ data: `${body.City} ${body.Date} ${body.Rainfall_30_Days} ${body.Rainfall_60_Days} ${body.Rainfall_90_Days}` });
 }

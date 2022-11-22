0ad6f8890d11595309b64f53a1d754c74daec9b8
JackBui00 <jackbui05@hotmail.com>
2022-11-22 12:13:11 -0800

    display input

--- pages/api/forms.js
+++ pages/api/forms.js
@@ -6,21 +6,21 @@ export default function handler(req, res) {
   const body = req.body;
   console.log(typeof body);
 
-  fs.createReadStream('public/fireTestData.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-      console.log(row);
-      //console.log(row)
+  // fs.createReadStream('public/fireTestData.csv')
+  //   .pipe(csv())
+  //   .on('data', (row) => {
+  //     console.log(row);
+  //     //console.log(row)
 
-      //console.log(row)
-      //if(row.Date === body.Date & row.City === body.City){
-      //    console.log(row)
-      //}
-    })
-    .on('end', () => {});
+  //     //console.log(row)
+  //     //if(row.Date === body.Date & row.City === body.City){
+  //     //    console.log(row)
+  //     //}
+  //   })
+  //   .on('end', () => {});
 
   console.log(req.body);
 
   //res.status(200).json({csvData})
-  res.status(200).json({ data: `${body.City} ${body.Date} ${body.Humidity}` });
+  res.status(200).json({ data: `${body.City} ${body.Date} ${body.Rainfall_30_Days} ${body.Rainfall_60_Days} ${body.Rainfall_60_Days}` });
 }
